2d56d4c3fff5e7aab587056478fe6bc33bc24836
Maartje Eyskens <meyskens@me.com>
2016-05-06 23:22:15 +0200

    Add enabled boolean

--- components/dj/separation.js
+++ components/dj/separation.js
@@ -2,6 +2,7 @@ var mongoose = requireFromRoot("components/database/mongodb.js")
 var Schema = mongoose.Schema
 var ObjectId = mongoose.Types.ObjectId
 var separationSchema = new Schema({
+    enabled: Boolean,
     username: String,
 	separationType: String, // artist, song
     interval: Number,
