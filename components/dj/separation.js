50a80721ee29eb9a26f1b1c7f7ef2d6bc2fdf7d0
Maartje Eyskens <meyskens@me.com>
2016-05-06 23:08:43 +0200

    Add separation rules Schema

---
+++ components/dj/separation.js
@@ -0,0 +1,16 @@
+var mongoose = requireFromRoot("components/database/mongodb.js")
+var Schema = mongoose.Schema
+var ObjectId = mongoose.Types.ObjectId
+var separationSchema = new Schema({
+    username: String,
+	separationType: String, // artist, song
+    interval: Number,
+}, { collection: "dj_separation" })
+separationSchema.index({
+    username: 1,
+});
+var separationModel = mongoose.model("dj_separation", separationSchema, "dj_separation")
+
+module.exports.separationForUsername = function (username, callback) {
+    separationModel.find({username: username}, callback)
+}
