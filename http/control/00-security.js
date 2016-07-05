051f0ad20011287626ae3e06767e466dc219aede
Léo Lam <leo@innovatetechnologi.es>
2016-07-05 15:13:17 +0200

    controlUser: Switch to async/await

--- http/control/00-security.js
+++ http/control/00-security.js
@@ -1,15 +1,15 @@
-var wait = require("wait.for")
-var fs = require("fs")
-var _ = require("underscore")
+import fs from "fs";
+import wait from "wait.for";
+import _ from "underscore";
 
-import BadRequestError from "~/http/classes/BadRequestError"
+import BadRequestError from "~/http/classes/BadRequestError";
 
-var InvalidatedTokens = requireFromRoot("components/control/invalidatedTokensDatabase.js")
-var controlUser = requireFromRoot("components/control/controlUser.js")
-var publicKey = fs.readFileSync(global.appRoot + "/keys/controlPublicKey.pem")
-var cert = fs.readFileSync(global.appRoot + "/keys/controlSigningKey.pem")
+const InvalidatedTokens = requireFromRoot("components/control/invalidatedTokensDatabase.js");
+const controlUser = requireFromRoot("components/control/controlUser.js");
+const publicKey = fs.readFileSync(global.appRoot + "/keys/controlPublicKey.pem");
+const cert = fs.readFileSync(global.appRoot + "/keys/controlSigningKey.pem");
 
-module.exports = function ({ app, expressJwt, jwt }) {
+export default function ({ app, expressJwt, jwt, wrap }) {
     app.use("/control", expressJwt({
         secret: publicKey,
         audience: "https://itframe.shoutca.st/control",
@@ -22,7 +22,7 @@ module.exports = function ({ app, expressJwt, jwt }) {
                 return next(new BadRequestError("No token was found in the Authorization header."));
             }
             if (wait.for(InvalidatedTokens.isTokenInvalidated, token)) {
-                req.log.info(`Found invalidated token, sending 419`);
+                req.log.info("Found invalidated token, sending 419");
                 return res.status(419).json({
                     result: "error",
                     error: "The token has been invalidated.",
@@ -55,103 +55,93 @@ module.exports = function ({ app, expressJwt, jwt }) {
         });
     });
 
-    app.all("/control/*", function checkService(req, res, next) {
-        wait.launchFiber(() => {
-            if (!req.query.username && !req.body.username) {
-                return next();
-            }
+    app.all("/control/*", wrap(async (req, res, next) => {
+        if (!req.query.username && !req.body.username) {
+            return next();
+        }
 
-            if (req.query.username && req.body.username) {
-                return next(new BadRequestError("Only one username was expected."));
-            }
+        if (req.query.username && req.body.username) {
+            throw new BadRequestError("Only one username was expected.");
+        }
 
-            let usernameToCheck = req.query.username || req.body.username;
-            let products = wait.for(controlUser.getProductsForEmail, req.user.email);
-            let product = _.findWhere(products, {
-                username: usernameToCheck,
-            });
-            if (!product) {
-                return next(new BadRequestError("The service mentioned in the request isn't valid."));
-            }
-            req.log = req.log.child({
-                req: {
-                    method: req.method,
-                    url: req.url,
-                    ip: req.ip,
-                    user: req.user,
-                    product,
-                },
-            });
-            req.product = product;
-            return next(); // allow request to continue
+        const usernameToCheck = req.query.username || req.body.username;
+        const products = await controlUser.getProductsForEmail(req.user.email);
+        const product = products.find(p => p.username === usernameToCheck);
+        if (!product) {
+            throw new BadRequestError("The service mentioned in the request isn't valid.");
+        }
+        req.log = req.log.child({
+            req: {
+                method: req.method,
+                url: req.url,
+                ip: req.ip,
+                user: req.user,
+                product,
+            },
         });
-    });
+        req.product = product;
+        return next(); // allow request to continue
+    }));
 
-    app.param("username", function (req, res, next, username) {
+    app.param("username", wrap(async (req, res, next, username) => {
         if (!req.path.includes("/control/")) {
             return next();
         }
-        wait.launchFiber(() => {
-            if ((req.query.username && username && req.query.username !== username)
-                || (req.body.username && username && req.body.username !== username)) {
-                return next(new BadRequestError("Only one username was expected."));
-            }
+        if ((req.query.username && username && req.query.username !== username)
+            || (req.body.username && username && req.body.username !== username)) {
+            throw new BadRequestError("Only one username was expected.");
+        }
 
-            let products = wait.for(controlUser.getProductsForEmail, req.user.email);
-            let product = _.findWhere(products, { username });
-            if (!product) {
-                return next(new BadRequestError("The service mentioned in the request isn't valid."));
-            }
-            req.log = req.log.child({
-                req: {
-                    method: req.method,
-                    url: req.url,
-                    ip: req.ip,
-                    user: req.user,
-                    product,
-                },
-            });
-            req.product = product;
-            return next(); // allow request to continue
+        const products = await controlUser.getProductsForEmail(req.user.email);
+        const product = products.find(p => p.username === username);
+        if (!product) {
+            throw new BadRequestError("The service mentioned in the request isn't valid.");
+        }
+        req.log = req.log.child({
+            req: {
+                method: req.method,
+                url: req.url,
+                ip: req.ip,
+                user: req.user,
+                product,
+            },
         });
-    });
+        req.product = product;
+        return next(); // allow request to continue
+    }));
 
-    app.post("/authenticate", function (req, res) {
-        wait.launchFiber(() => {
-            if (typeof req.body.email === "undefined" || typeof req.body.password === "undefined") {
-                throw new BadRequestError("Missing data");
-            }
-            let correct = wait.for(controlUser.checkLogin, req.body.email, req.body.password);
-            if (!correct) {
-                return res.status(401).json({
-                    error: "Wrong login",
-                    result: "error",
-                });
-            }
-
-            let token = jwt.sign({
-                email: req.body.email,
-                test: "ok",
-            }, cert, {
-                expiresInMinutes: 60,
-                audience: "https://itframe.shoutca.st/control",
-                algorithm: "RS256",
+    app.post("/authenticate", wrap(async (req, res) => {
+        if (typeof req.body.email === "undefined" || typeof req.body.password === "undefined") {
+            throw new BadRequestError("Missing data");
+        }
+        const correct = await controlUser.checkLogin(req.body.email, req.body.password);
+        if (!correct) {
+            return res.status(401).json({
+                error: "Wrong login",
+                result: "error",
             });
-            res.json({ token });
-        });
-    })
+        }
 
-    app.post("/control/keep-alive", function (req, res) {
-        wait.launchFiber(() => {
-            let data = wait.for(controlUser.getInfoForEmail, req.user.email);
-            let token = jwt.sign({ email: data.email }, cert, {
-                expiresInMinutes: 120,
-                audience: "https://itframe.shoutca.st/control",
-                algorithm: "RS256",
-            });
-            res.json({ token });
+        const token = jwt.sign({
+            email: req.body.email,
+            test: "ok",
+        }, cert, {
+            expiresInMinutes: 60,
+            audience: "https://itframe.shoutca.st/control",
+            algorithm: "RS256",
         });
-    });
+        res.json({ token });
+    }));
+
+    app.post("/control/keep-alive", wrap(async (req, res) => {
+        const data = await controlUser.getInfoForEmail(req.user.email);
+        const token = jwt.sign({ email: data.email }, cert, {
+            expiresInMinutes: 120,
+            audience: "https://itframe.shoutca.st/control",
+            algorithm: "RS256",
+        });
+        res.json({ token });
+    }));
 
     app.post("/control/log-out", function (req, res) {
         wait.launchFiber(() => {
