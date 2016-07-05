b823ee77d9ae060ab59b0e57d2de9f917bd9c6ed
Léo Lam <leo@innovatetechnologi.es>
2016-07-05 15:38:38 +0200

    controlUser, WHMCS: Add support-related methods

--- components/control/controlUser.js
+++ components/control/controlUser.js
@@ -29,6 +29,30 @@ export const getProductsForEmail = function (email) {
     return userBackend.getProductsForEmail(email);
 };
 
+
+/**
+ * Get a ticket by ticket ID
+ * @param  {String} email         Email address
+ * @param  {String} ticketId
+ * @return {Object} Ticket object
+ */
+export const getTicket = (email, ticketId) => {
+    if (!ticketId) {
+        return Promise.reject("No ticket ID");
+    }
+    return userBackend.getTicket(email, ticketId);
+};
+
+/**
+ * Get all tickets for an user
+ * @param  {String}  email         Email address
+ * @param  {Boolean} activeOnly    Get active tickets only
+ * @return {Array}   Array of ticket objects
+ */
+export const getTickets = (email, activeOnly = false) => {
+    return userBackend.getTickets(email, activeOnly);
+};
+
 /**
  * Open a ticket on behalf of the user
  * @param  {String}   options.email        Client's email address
@@ -39,6 +63,9 @@ export const getProductsForEmail = function (email) {
  * @param  {Boolean}  options.sendEmail    Send an email about the ticket to the client
  */
 export const openTicket = function ({ email, departmentId, subject, message, sendEmail } = {}) {
+    if (!departmentId || !subject || !message) {
+        return Promise.reject("Missing fields");
+    }
     return userBackend.openTicket({
         email,
         departmentId,
@@ -47,6 +74,50 @@ export const openTicket = function ({ email, departmentId, subject, message, sen
         sendEmail,
     });
 };
+
+/**
+ * Change a ticket status by ticket ID
+ * @param  {String}   email
+ * @param  {String}   ticketId     Ticket ID
+ * @param  {String}   status       New status
+ */
+export const changeStatusForTicket = function (email, ticketId, status) {
+    if (!ticketId) {
+        return Promise.reject("No ticket ID");
+    }
+    if (!status || ["closed", "open"].indexOf(status) === -1) {
+        return Promise.reject("No status, or invalid status");
+    }
+    return userBackend.changeStatusForTicket(email, ticketId, status);
+};
+
+
+/**
+ * Get replies for a ticket by ticket ID
+ * @param  {String} email
+ * @param  {String} ticketId
+ * @return {Array}  Array of replies object
+ */
+export const getRepliesForTicket = (email, ticketId) => {
+    if (!ticketId) {
+        return Promise.reject("No ticket ID");
+    }
+    return userBackend.getRepliesForTicket(email, ticketId);
+};
+
+/**
+ * Post a reply on a ticket on behalf of the user
+ * @param  {String}   email
+ * @param  {String}   ticketId     Ticket ID
+ * @param  {String}   message      Reply message
+ */
+export const postReplyForTicket = (email, ticketId, message) => {
+    if (!ticketId || !message) {
+        return Promise.reject("No ticket ID or message");
+    }
+    return userBackend.postReplyForTicket(email, ticketId, message);
+};
+
 
 /**
  * Send an email related to a product
