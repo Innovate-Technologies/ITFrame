273150d0e726628a6ac0d7bf94077d8ca8c47d23
Léo Lam <leo@innovatetechnologi.es>
2016-07-05 15:13:17 +0200

    controlUser: Switch to async/await

--- http/internal/apps.js
+++ http/internal/apps.js
@@ -1,17 +1,19 @@
-let promisify = require("promisify-node");
-let users = requireFromRoot("components/legacy/usersDatabase.js");
-let controlUser = requireFromRoot("components/control/controlUser.js");
-let legacyiOSDatabase = requireFromRoot("components/iOS/legacyDatabase.js");
-let castDatabase = requireFromRoot("components/cast/database.js");
-let nowPlayingDatabase = promisify(requireFromRoot("components/nowplaying/nowPlayingDatabase.js"), undefined, true);
-let legacyNowPlaying = promisify(requireFromRoot("components/nowplaying/legacyNowPlaying.js"), undefined, true);
-let AppsService = requireFromRoot("components/apps/api.js");
-let profiler = requireFromRoot("profiler");
+import promisify from "promisify-node";
 
 import BadRequestError from "~/http/classes/BadRequestError";
 import AccessDeniedError from "~/http/classes/AccessDeniedError";
+const users = requireFromRoot("components/legacy/usersDatabase.js");
+const controlUser = requireFromRoot("components/control/controlUser.js");
+const legacyiOSDatabase = requireFromRoot("components/iOS/legacyDatabase.js");
+const castDatabase = requireFromRoot("components/cast/database.js");
+const nowPlayingDatabase = promisify(requireFromRoot("components/nowplaying/nowPlayingDatabase.js"),
+    undefined, true);
+const legacyNowPlaying = promisify(requireFromRoot("components/nowplaying/legacyNowPlaying.js"),
+    undefined, true);
+const AppsService = requireFromRoot("components/apps/api.js");
+const profiler = requireFromRoot("profiler");
 
-let moduleLogger = log.child({ component: "internal/apps" });
+const moduleLogger = log.child({ component: "internal/apps" });
 
 export default ({ app, wrap }) => {
     const appsToken = config.appsToken;
@@ -134,67 +136,51 @@ export default ({ app, wrap }) => {
     // Notification emails
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    app.post("/internal/apps/notify-client-android-app-done", (req, res, next) => {
+    app.post("/internal/apps/notify-client-android-app-done", wrap(async (req, res) => {
         if (req.body.token !== appsToken) {
             throw new AccessDeniedError();
         }
         if (!req.body.username) {
             throw new BadRequestError("Missing username");
         }
-        controlUser.sendProductEmail("Android App Submitted", req.body.username, {}, function (err) {
-            if (err) {
-                return next(err);
-            }
-            return res.json({});
-        });
-    });
+        await controlUser.sendProductEmail("Android App Submitted", req.body.username, {});
+        res.json({});
+    }));
 
-    app.post("/internal/apps/notify-client-iOS-app-done", (req, res, next) => {
+    app.post("/internal/apps/notify-client-iOS-app-done", wrap(async (req, res) => {
         if (req.body.token !== appsToken) {
             throw new AccessDeniedError();
         }
         if (!req.body.username) {
             throw new BadRequestError("Missing username");
         }
-        controlUser.sendProductEmail("iOS App Submitted", req.body.username, {}, function (err) {
-            if (err) {
-                return next(err);
-            }
-            return res.json({});
-        });
-    });
+        await controlUser.sendProductEmail("iOS App Submitted", req.body.username, {});
+        res.json({});
+    }));
 
-    app.post("/internal/apps/notify-client-iOS-app-approved", (req, res, next) => {
+    app.post("/internal/apps/notify-client-iOS-app-approved", wrap(async (req, res) => {
         if (req.body.token !== appsToken) {
             throw new AccessDeniedError();
         }
         if (!req.body.username) {
             throw new BadRequestError("Missing username");
         }
-        controlUser.sendProductEmail("iOS App Approved", req.body.username, {}, function (err) {
-            if (err) {
-                return next(err);
-            }
-            return res.json({});
-        });
-    });
+        await controlUser.sendProductEmail("iOS App Approved", req.body.username, {});
+        res.json({});
+    }));
 
-    app.post("/internal/apps/notify-client-app-rejected", (req, res, next) => {
+    app.post("/internal/apps/notify-client-app-rejected", wrap(async (req, res) => {
         if (req.body.token !== appsToken) {
             throw new AccessDeniedError();
         }
         if (!req.body.username || !req.body.reason) {
             throw new BadRequestError("Missing username and/or reason");
         }
-        controlUser.sendProductEmail("App Request Rejected", req.body.username, {
+        await controlUser.sendProductEmail("App Request Rejected", req.body.username, {
             reason: req.body.reason,
-        }, function (err) {
-            if (err) {
-                return next(err);
-            }
-            return res.json({});
         });
-    });
+        res.json({});
+    }));
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
