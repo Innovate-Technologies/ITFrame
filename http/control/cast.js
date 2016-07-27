54068a07c472cc95b353afa8acfa811ffe7758ad
Maartje Eyskens <meyskens@me.com>
2016-07-27 09:04:10 +0200

    Fix feedback

--- http/control/cast.js
+++ http/control/cast.js
@@ -141,20 +141,7 @@ module.exports = ({ app }) => {
     app.put("/control/cast/anti-stream-ripper/:username", (req, res, next) => {
         wait.launchFiber(function () {
             try {
-                wait.for(cast.setAntiStreamRipper, true);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to configure streams");
-                error.message = "Failed to configure streams: " + error.message;
-                return next(error);
-            }
-        });
-    });
-
-    app.del("/control/cast/anti-stream-ripper/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.setAntiStreamRipper, false);
+                wait.for(cast.setAntiStreamRipper, req.body.isEnabled);
                 res.json({});
             } catch (error) {
                 req.log.warn(error, "Failed to configure streams");
@@ -167,20 +154,7 @@ module.exports = ({ app }) => {
     app.put("/control/cast/hide-listener-count/:username", (req, res, next) => {
         wait.launchFiber(function () {
             try {
-                wait.for(cast.setHideListenerCount, true);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to configure streams");
-                error.message = "Failed to configure streams: " + error.message;
-                return next(error);
-            }
-        });
-    });
-
-    app.del("/control/cast/hide-listener-count/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.setHideListenerCount, false);
+                wait.for(cast.setHideListenerCount, req.body.isEnabled);
                 res.json({});
             } catch (error) {
                 req.log.warn(error, "Failed to configure streams");
