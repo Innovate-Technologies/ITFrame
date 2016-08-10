97e7f3580afa144e273c7ce7f070fd7a88bca659
Maartje Eyskens <meyskens@me.com>
2016-08-10 11:50:31 +0200

    refactor lookUp

--- components/tunes/lookup.js
+++ components/tunes/lookup.js
@@ -1,49 +1,27 @@
-var iTunes = require("./iTunesAPI.js")
-var personalDB = require("./personalMusicDatabase.js")
+import * as iTunes from "./iTunesAPI.js"
+import * as personalDB from "./personalMusicDatabase.js"
 
+export const lookUp = async (username, song, artist) => {
+    const songInDatabase = await personalDB.getSongForUsername(username, song, artist)
+    if (songInDatabase) {
+        return songInDatabase
+    }
 
-var lookUp = function (username, song, artist, callback) {
-    personalDB.getSongForUsername(username, song, artist, function (err, res) {
-        if (err) {
-            callback(err)
-            return
-        }
-        if (res !== null) {
-            callback(null, res)
-            return
-        }
-        iTunes.searchTitle(song, artist, 1, function (searchErr, searchRes) {
-            if (searchErr) {
-                return callback(null, {
-                    song: song,
-                    artist: artist,
-                    externalURL: {}
-                })
-            }
-            var info;
-            if (searchRes.length === 0) {
-                info = {
+    const songInfo = {
                     song: song,
                     artist: artist,
                     externalURL: {}
                 }
-                personalDB.addSong(username, info)
-                return callback(null, info)
-            }
-            info = {
-                song: song,
-                artist: artist,
-                album: searchRes[0].collectionName,
-                externalURL: {
-                    "iTunes": searchRes[0].trackViewUrl
-                }, //{itunes:url} (Would have called it buy url but since streaming)
-                artwork: "https://photon.shoutca.st/" + ((searchRes[0].artworkUrl100 || "").replace("100x100", "1200x1200")).replace("https://", "").replace("http://", ""),
-                genre: searchRes[0].primaryGenreName
-            }
-            personalDB.addSong(username, info)
-            return callback(null, info)
-        })
-    })
-}
 
-module.exports.lookUp = lookUp
+    if (song && artist) {
+        const iTunesResults = await iTunes.searchTitle(song, artist, 1)
+        if (iTunesResults.length !== 0) {
+            songInfo.album = searchRes[0].collectionName
+            songInfo.externalURL = { "itunes": searchRes[0].trackViewUrl }
+            songInfo.artwork=  "https://photon.shoutca.st/" + ((searchRes[0].artworkUrl100 || "").replace("100x100", "1200x1200")).replace("https://", "").replace("http://", "")
+            songInfo.genre = searchRes[0].primaryGenreName
+        }
+    }
+    await personalDB.addSong(username, info)
+    return info
+}
