811219a0d38deb2cd0ec240850b80cbf38d908b9
Maartje Eyskens <meyskens@me.com>
2016-08-10 12:12:06 +0200

    Refactor now playing handle

--- components/nowplaying/handle.js
+++ components/nowplaying/handle.js
@@ -2,61 +2,25 @@ var database = require("./nowPlayingDatabase.js")
 var tunes = requireFromRoot("components/tunes/lookup.js")
 let logger = log.child({ component: "NP Handle" });
 
-module.exports = function (info) {
-    database.getLatestSong(info.username, (err, latestSongInDatabase) => {
-        if (err) {
-            return
-        }
-        if (latestSongInDatabase.song === info.title && latestSongInDatabase.artist === info.artist) {
-            return
-        }
-        logger.debug(info.username, "Checking tunes")
-        tunes.lookUp(info.username, info.title, info.artist, (tunesErr, tunesSongInfo) => {
-            if (tunesErr) {
-                return
-            }
-            logger.debug(info.username, "Creating entry")
-            var entry = {
-                username: info.username,
-                song: tunesSongInfo.title || info.title,
-                artist: tunesSongInfo.artist || info.artist,
-                cover: tunesSongInfo.artwork || "https://cdn.shoutca.st/noalbum.png",
-                wiki: "",
-                buy: (tunesSongInfo.external_url || {}).iTunes || "",
-                time: Math.round((new Date()).getTime() / 1000),
-            }
-            database.addSong(entry, () => {
-                logger.debug(info.username, "Call hooks")
-                global.hooks.runHooks("newSong", entry)
-            })
-        })
-    })
-    // wait.launchFiber(handleNowPlaying, info);
-}
-/* var wait = require("wait.for")
-
-module.exports = function (info) {
-    // wait.launchFiber(handleNowPlaying, info);
-}
-
-var handleNowPlaying = function (info) {
-    var latestSongInDatabase = wait.for(database.getLatestSong, info.username)
-    if (latestSongInDatabase.song === info.title && latestSongInDatabase.artist === info.artist) {
+export default async (info) => {
+    const latestSong = await database.getLatestSong(info.username)
+    if (latestSong && latestSong.song === info.title && latestSong.artist === info.artist) {
         return
     }
-    var tunesSongInfo = wait.for(tunes.lookUp, info.username, info.title, info.artist)
+    logger.debug(info.username, "Checking tunes")
+    const tunesInfo = tunes.lookUp(info.username, info.title, info.artist)
 
-    var entry = {
+    const entry = {
         username: info.username,
-        song: tunesSongInfo.title || info.title,
-        artist: tunesSongInfo.artist || info.artist,
-        cover: tunesSongInfo.artwork || "https://cdn.shoutca.st/noalbum.png",
+        song: tunesInfo.title,
+        artist: tunesInfo.artist,
+        cover: tunesInfo.artwork,
         wiki: "",
-        buy: (tunesSongInfo.external_url || {}).iTunes || "",
+        buy: (tunesSongInfo.external_url || {}).itunes || "",
         time: Math.round((new Date()).getTime() / 1000),
     }
-
-    if (wait.for(database.addSong, entry)) {
-        global.hooks.runHooks("newSong", entry)
-    }
-}*/
+    logger.debug(info.username, "Saving song")
+    await database.addSong(entry)
+    logger.debug(info.username, "Triggering hooks")
+    global.hooks.runHooks("newSong", entry)
+}
