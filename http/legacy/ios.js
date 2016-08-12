ec7084e65cac80056d3a6b032d7e2ccbf6ee9af7
Maartje Eyskens <meyskens@me.com>
2016-08-12 13:55:56 +0200

    Change all instances using callback on nowplaying

--- http/legacy/ios.js
+++ http/legacy/ios.js
@@ -1,9 +1,9 @@
-var iOSDatabase = requireFromRoot("components/iOS/legacyDatabase.js")
-var NowPlaying = requireFromRoot("components/nowplaying/nowPlayingDatabase.js")
-var iOS = requireFromRoot("components/iOS/legacy.js")
+const iOSDatabase = requireFromRoot("components/iOS/legacyDatabase.js")
+const nowPlaying = requireFromRoot("components/nowplaying/nowPlayingDatabase.js")
+const iOS = requireFromRoot("components/iOS/legacy.js")
 
-module.exports = function (parm) {
-    parm.app.get("/mobile/iOS/", function (req, res) {
+module.exports = function ({ app, wrap}) {
+    app.get("/mobile/iOS/", function (req, res) {
         if (typeof req.query.sku === "undefined") {
             return res.status(400).send("Missing SKU")
         }
@@ -16,53 +16,49 @@ module.exports = function (parm) {
         })
     });
 
-    parm.app.get("/mobile/iOS/nowplaying/", function (req, res) {
+    app.get("/mobile/iOS/nowplaying/", wrap((req, res) => {
         if (typeof req.query.sku === "undefined") {
             return res.status(400).send("Missing SKU")
         }
-        iOSDatabase.getAppForSKU(req.query.sku, function (err, dbres) {
+        iOSDatabase.getAppForSKU(req.query.sku, async (err, dbres) => {
             if (err) {
                 return res.status(500).json({ error: err })
             }
-            NowPlaying.getLatestSongs(dbres.username, 5, function (error, np) {
-                if (error) {
-                    return res.status(500).json({ error: error })
-                }
-                if (np.length === 0) {
-                    return res.json([{
-                        "song": "Unknown",
-                        "artist": "Unknown",
-                        "cover": "https://cdn.shoutca.st/noalbum.png",
-                        "buy": "",
-                        "wiki": "Coming Soon",
-                    }])
-                }
-                res.json(np)
-            })
+            const np = await nowPlaying.getLatestSongs(dbres.username, 5)
+            if (np.length === 0) {
+                return res.json([{
+                    "song": "Unknown",
+                    "artist": "Unknown",
+                    "cover": "https://cdn.shoutca.st/noalbum.png",
+                    "buy": "",
+                    "wiki": "Coming Soon",
+                }])
+            }
+            res.json(np)
         })
     })
 
-    parm.app.get("/mobile/iOS/tunein/", function (req, res) {
-        if (typeof req.query.sku === "undefined") {
-            return res.status(400).json({
-                error: "Missing parameters",
-            })
-        }
-
-        iOSDatabase.getAppForSKU(req.query.sku, function (err, app) {
-            if (err) {
-                return res.status(500).json({ error: err })
+    app.get("/mobile/iOS/tunein/", function (req, res) {
+            if (typeof req.query.sku === "undefined") {
+                return res.status(400).json({
+                    error: "Missing parameters",
+                })
             }
-            iOS.getPlsForUsername(app.username, function (error, link) {
-                if (error) {
-                    return res.status(500).json({ error })
-                }
-                res.json({ link })
-            })
-        })
-    });
 
-    parm.app.get("/mobile/iOS/nowplayingForUser/", function (req, res) {
+            iOSDatabase.getAppForSKU(req.query.sku, function (err, app) {
+                if (err) {
+                    return res.status(500).json({ error: err })
+                }
+                iOS.getPlsForUsername(app.username, function (error, link) {
+                    if (error) {
+                        return res.status(500).json({ error })
+                    }
+                    res.json({ link })
+                })
+            })
+        });
+
+    app.get("/mobile/iOS/nowplayingForUser/", function (req, res) {
         if (typeof req.query.user === "undefined") {
             return res.status(400).send("Missing Username")
         }
--- http/public/nowplaying.js
+++ http/public/nowplaying.js
@@ -1,17 +1,11 @@
-/* global requireFromRoot */
-let NowPlaying = requireFromRoot("components/nowplaying/nowPlayingDatabase.js")
+const nowPlaying = requireFromRoot("components/nowplaying/nowPlayingDatabase.js")
 
-module.exports = ({ app }) => {
-    app.get("/nowplaying/:username", function (req, res) {
+module.exports = ({ app, wrap }) => {
+    app.get("/nowplaying/:username", wrap(async (req, res) => {
         if (typeof req.params.username === "undefined") {
             return res.status(400).send("Missing Username")
         }
+        res.json(await nowPlaying.getLatestSongs(req.params.username, 10))
+    })
+}
 
-        NowPlaying.getLatestSongs(req.params.username, 10, function (error, np) {
-            if (error) {
-                return res.status(500).json({ error: error })
-            }
-            res.json(np)
-        })
-    });
-};
