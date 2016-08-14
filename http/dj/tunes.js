19676b8b1610feb5b691bfa1c026922d50403973
Maartje Eyskens <meyskens@me.com>
2016-08-14 10:49:01 +0200

    Fix errors in dj/tunes

--- http/dj/tunes.js
+++ http/dj/tunes.js
@@ -1,42 +1,42 @@
 var tunesDB = requireFromRoot("components/tunes/personalMusicDatabase.js")
 
 module.exports = function ({ app, wrap }) {
-	app.get("/dj/:user/:key/song/:id", wrap((req, res) => {
-		if (!req.params.id) {
-			return res.status(500).json({
-				result: "error",
+    app.get("/dj/:user/:key/song/:id", wrap((req, res) => {
+        if (!req.params.id) {
+            return res.status(500).json({
+                result: "error",
                 error: "Missing info.",
-			})
-		}
-		const song = tunesDB.getSongForUserWithID(req.params.user, req.params.id)
-		if (song === null) {
-			return res.status(500).json({
-				result: "error",
-				error: "Song not found",
-			})
-		}
-		res.json(song)
-	}))
+            })
+        }
+        const song = tunesDB.getSongForUserWithID(req.params.user, req.params.id)
+        if (song === null) {
+            return res.status(500).json({
+                result: "error",
+                error: "Song not found",
+            })
+        }
+        res.json(song)
+    }))
 
-	app.get("/dj/:user/:key/songs-with-tag/:tag", wrap((req, res) => {
-		if (!req.params.tag) {
-			return res.status(500).json({
-				result: "error",
+    app.get("/dj/:user/:key/songs-with-tag/:tag", wrap((req, res) => {
+        if (!req.params.tag) {
+            return res.status(500).json({
+                result: "error",
                 error: "Missing info.",
-			})
-		}
+            })
+        }
 
-		const songs = tunesDB.getSongsForUserWithTag(req.params.user, req.params.tag)
-		if (song === null) {
-			return res.status(500).json({
-				result: "error",
-				error: "Songs not found",
-			})
-		}
-		res.json(song)
-	}))
+        const songs = tunesDB.getSongsForUserWithTag(req.params.user, req.params.tag)
+        if (songs === null) {
+            return res.status(500).json({
+                result: "error",
+                error: "Songs not found",
+            })
+        }
+        res.json(songs)
+    }))
 
-	app.get("/dj/:user/:key/all-songs/", wrap((req, res) => {
-		res.json(await tunesDB.getAllSongsForUser(req.params.user))
-	}))
+    app.get("/dj/:user/:key/all-songs/", wrap(async (req, res) => {
+        res.json(await tunesDB.getAllSongsForUser(req.params.user))
+    }))
 }
