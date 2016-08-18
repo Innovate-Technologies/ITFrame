291e88eacd197e220fd7d3c21572133403b01cd5
Maartje Eyskens <meyskens@me.com>
2016-08-18 17:44:58 +0200

    Don't always throw errors

--- http/public/nowplaying.js
+++ http/public/nowplaying.js
@@ -3,7 +3,9 @@ const nowPlaying = requireFromRoot("components/nowplaying/nowPlayingDatabase.js"
 
 module.exports = ({ app, wrap }) => {
     app.get("/nowplaying/:username", wrap(async (req, res) => {
-        throw new BadRequestError("Missing username")
+        if (!req.params.username) {
+            throw new BadRequestError("Missing username")
+        }
         res.json(await nowPlaying.getLatestSongs(req.params.username, 10))
     }))
 }
