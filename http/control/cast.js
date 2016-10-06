6571fdf9c5cf4600ccae0a871e8149178b600461
Maartje Eyskens <meyskens@me.com>
2016-10-06 21:16:33 +0200

    Replace wait.for code missed in Cast PR

--- http/control/cast.js
+++ http/control/cast.js
@@ -1,167 +1,81 @@
-/* global requireFromRoot */
-let wait = require("wait.for");
-let cast = requireFromRoot("components/cast/manage.js");
-let castDatabase = requireFromRoot("components/cast/database.js");
+import * as cast from "../../components/cast/manage.js"
+import * as castDatabase from "../../components/cast/database.js"
 import NotFoundError from "~/http/classes/NotFoundError";
 import BadRequestError from "~/http/classes/BadRequestError";
 
-module.exports = ({ app }) => {
+module.exports = ({ app, wrap }) => {
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Configuration
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    app.get("/control/cast/configuration/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                let castConfig = wait.for(castDatabase.getInfoForUsername, req.params.username);
-                castConfig.internal = {};
-                res.json(castConfig);
-            } catch (error) {
-                req.log.warn(error, "Failed to get config");
-                return next(new NotFoundError("Failed to get config: " + error.message));
-            }
-        });
-    });
+    app.get("/control/cast/configuration/:username", wrap(async (req, res) => {
+        const castConfig = await castDatabase.getInfoForUsername(req.params.username)
+        delete castConfig.internal
+        res.json(castConfig)
+    }));
 
-    app.post("/control/cast/upgrade/:username", function (req, res, next) {
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.upgradeNode, req.params.username);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to upgrade");
-                error.message = "Failed to upgrade: " + error.message;
-                return next(error);
-            }
-        });
-    });
+    app.post("/control/cast/upgrade/:username", wrap(async (req, res) => {
+        await cast.upgradeNode(req.params.username)
+        res.json({})
+    }));
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Directories
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    app.get("/control/cast/directories/get-supported", function (req, res) {
+    app.get("/control/cast/directories/get-supported", (req, res) => {
         res.json(cast.supportedDirectories);
     });
 
-    app.post("/control/cast/directories/enable/:username", function (req, res, next) {
+    app.post("/control/cast/directories/enable/:username", wrap(async (req, res) => {
         if (!req.body.directory) {
             throw new BadRequestError();
         }
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.addToDirectory, req.params.username, req.body.directory);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to enable directory");
-                error.message = "Failed to enable directory: " + error.message;
-                return next(error);
-            }
-        });
-    });
+        await cast.addToDirectory(req.params.username, req.body.directory)
+        res.json({})
+    }));
 
-    app.post("/control/cast/directories/disable/:username", function (req, res, next) {
+    app.post("/control/cast/directories/disable/:username", wrap(async (req, res) => {
         if (!req.body.directory) {
             throw new BadRequestError();
         }
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.removeFromDirectory, req.params.username, req.body.directory);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to disable directory");
-                error.message = "Failed to disable directory: " + error.message;
-                return next(error);
-            }
-        });
-    });
+        await cast.removeFromDirectory(req.params.username, req.body.directory)
+        res.json({})
+    }));
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Streams
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    app.put("/control/cast/streams/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.configureStreams, req.params.username, req.body);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to configure streams");
-                error.message = "Failed to configure streams: " + error.message;
-                return next(error);
-            }
-        });
-    });
-
-    app.put("/control/cast/configure-dj/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                /*
-                    {
-                        enabled: Boolean,
-                        name: String,
-                        genre: String,
-                        fadeLength: Number,
-                    }
-                */
-                wait.for(cast.configureDJ, req.params.username, req.body);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to configure DJ");
-                error.message = "Failed to configure DJ: " + error.message;
-                return next(error);
-            }
-        });
-    });
+    app.put("/control/cast/streams/:username", wrap(async (req, res) => {
+        await cast.configureStreams(req.params.username, req.body)
+        res.json({})
+    }));
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // GeoLock
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    app.put("/control/cast/geolock/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.setGeoLock, req.params.username, req.body);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to configure streams");
-                error.message = "Failed to configure streams: " + error.message;
-                return next(error);
-            }
-        });
-    });
+    app.put("/control/cast/geolock/:username", wrap(async (req, res) => {
+        await cast.setGeoLock(req.params.username, req.body)
+        res.json({})
+    }));
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Extra features
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    app.put("/control/cast/anti-stream-ripper/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.setAntiStreamRipper, req.params.username, req.body.isEnabled);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to configure streams");
-                error.message = "Failed to configure streams: " + error.message;
-                return next(error);
-            }
-        });
-    });
+    app.put("/control/cast/anti-stream-ripper/:username", wrap(async (req, res) => {
+        await cast.setAntiStreamRipper(req.params.username, req.body.isEnabled)
+        res.json({})
+    }));
 
-    app.put("/control/cast/hide-listener-count/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                wait.for(cast.setHideListenerCount, req.params.username, req.body.isEnabled);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to configure streams");
-                error.message = "Failed to configure streams: " + error.message;
-                return next(error);
-            }
-        });
-    });
+    app.put("/control/cast/hide-listener-count/:username", wrap(async (req, res) => {
+        await cast.setHideListenerCount(req.params.username, req.body.isEnabled)
+        res.json({})
+    }));
 
 };
