f45a386864cbbb3bcc69026c0cc39efe5a199aed
Maartje Eyskens <meyskens@me.com>
2017-05-20 22:40:30 +0200

    only push tags if exist

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker tag innovate/itframe:latest innovate/itframe:$TRAVIS_TAG
 docker push innovate/itframe:latest
-docker push innovate/itframe:$TRAVIS_TAG
+if [ ! -z "$TRAVIS_TAG" ]; then 
+    docker tag innovate/itframe:latest innovate/itframe:$TRAVIS_TAG
+    docker push innovate/itframe:$TRAVIS_TAG
+fi
