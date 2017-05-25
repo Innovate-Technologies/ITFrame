f6e400142bfdee22ac6f515cefaf4d4b51b76536
Maartje Eyskens <meyskens@me.com>
2017-05-25 14:39:39 +0200

    add staging deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push innovate/itframe:latest
+if [[ "$TRAVIS_BRANCH" == "master" ]] then
+    docker push innovate/itframe:latest
+fi
+if [[ "$TRAVIS_BRANCH" == "staging" ]] then
+    docker push innovate/itframe:staging
+fi
 if [ ! -z "$TRAVIS_TAG" ]; then 
     docker tag innovate/itframe:latest innovate/itframe:$TRAVIS_TAG
     docker push innovate/itframe:$TRAVIS_TAG
