4e53944ab169f54bf6bd162adcb4d6c828825456
Maartje Eyskens <meyskens@me.com>
2017-07-05 15:51:41 +0200

    rewrite tunes-worker to use dispatch

---
+++ src/components/dispatch/dispatch.js
@@ -0,0 +1,36 @@
+import rest from "restler"
+const logger = log.child({ component: "Dispatch" });
+
+let URL = ""
+
+export const setURL = (url) => {
+    this.url = url
+}
+
+export const newFromTempate = (template, name, vars = {}) => new Promise((resolve, reject) => {
+    rest.post(`${URL}/unit/from-template/${template}`, {
+        timeout: 10000,
+        data: {
+            name,
+            vars,
+        },
+    }).on("complete", function (returnData) {
+        logger.debug("Create succeeded");
+        resolve(returnData);
+    }).on("timeout", function () {
+        logger.error("Timeout");
+        reject(new Error("Time-out"))
+    })
+})
+
+export const destroy = (name) => new Promise((resolve, reject) => {
+    rest.del(`${URL}/unit/${name}`, {
+        timeout: 10000,
+    }).on("complete", function (returnData) {
+        logger.debug("Destroy succeeded");
+        resolve(returnData);
+    }).on("timeout", function () {
+        logger.error("Timeout");
+        reject(new Error("Time-out"))
+    })
+})
--- src/components/tunes/process.js
+++ src/components/tunes/process.js
@@ -1,92 +1,19 @@
-const fleet = requireFromRoot("components/coreos/fleet.js")
+import * as dispatch from "../dispatch/dispatch.js"
+
+dispatch.setURL(config.tunesLinkURL)
 
 export const processSong = async ({ id }) => {
-    const randomNumber = Math.floor(Math.random() * 4)
     try {
-        await createUnit(randomNumber, id)
+        await dispatch.newFromTempate("tunes-worker-*", id, { id })
     } catch (error) {
         processSong({ id })
     }
 }
 
-const createUnit = (randomNumber, id) => new Promise((resolve, reject) => {
-    fleet.newUnit(`tunes-worker-${randomNumber}-${id}.service`, {
-        desiredState: "launched",
-        name: `tunes-worker-${randomNumber}-${id}.service`,
-        options: [{
-            name: "Description",
-            section: "Unit",
-            value: "Tunes Worker for " + id,
-        }, {
-            "name": "After",
-            "section": "Unit",
-            "value": "docker.service",
-        }, {
-            "name": "EnvironmentFile",
-            "section": "Service",
-            "value": "/etc/environment",
-        }, {
-            "name": "TimeoutStartSec",
-            "section": "Service",
-            "value": "1m",
-        }, {
-            "name": "ExecStartPre",
-            "section": "Service",
-            "value": "-/bin/bash -c \"/usr/bin/docker pull docker.innovatete.ch/tunes-worker:`dpkg --print-architecture`-latest\"",
-        }, {
-            "name": "ExecStartPre",
-            "section": "Service",
-            "value": "-/usr/bin/docker kill tunes-" + id,
-        }, {
-            "name": "ExecStartPre",
-            "section": "Service",
-            "value": "-/usr/bin/docker rm tunes-" + id,
-        }, {
-            "name": "ExecStart",
-            "section": "Service",
-            value: `/bin/bash -c "/usr/bin/docker run --name 'tunes-${id}' -e ID='${id}' -e randomNumber='${randomNumber}' docker.innovatete.ch/tunes-worker:$(dpkg --print-architecture)-latest"`,
-        }, {
-            "name": "ExecStop",
-            "section": "Service",
-            "value": "/usr/bin/docker kill tunes-" + id,
-        }, {
-            "name": "ExecStopPost",
-            "section": "Service",
-            "value": "/usr/bin/docker rm tunes-" + id,
-        }, {
-            "name": "Restart",
-            "section": "Service",
-            "value": "always",
-        }, {
-            "name": "RestartSec",
-            "section": "Service",
-            "value": "5",
-        }, {
-            "name": "WantedBy",
-            "section": "Install",
-            "value": "multi-user.target",
-        }, {
-            "name": "MachineMetadata",
-            "section": "X-Fleet",
-            "value": "model=C2S",
-        }, {
-            "name": "MachineMetadata",
-            "section": "X-Fleet",
-            "value": "zone=ams1",
-        }, {
-            "name": "Conflicts",
-            "section": "X-Fleet",
-            "value": `tunes-worker-${randomNumber}*`,
-        }],
-    }, (err) => {
-        if (err) {
-            reject(err);
-        } else {
-            resolve();
-        }
-    })
-})
-
-export const stopContainer = (randomNumber, id) => {
-    fleet.destroyUnit(`tunes-worker-${randomNumber}-${id}.service`, Function.prototype)
+export const stopContainer = async (id) => {
+    try {
+        await dispatch.destroy(`tunes-worker-${id}`)
+    } catch (error) {
+        stopContainer({ id })
+    }
 }
