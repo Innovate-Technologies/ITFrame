195763480549795f79a18b2c9ea93e3433fd90a9
Maartje Eyskens <meyskens@me.com>
2017-07-09 16:15:21 +0200

    force id to string

--- src/components/tunes/process.js
+++ src/components/tunes/process.js
@@ -3,6 +3,7 @@ import { Dispatch } from "../dispatch/dispatch.js"
 const dispatch = new Dispatch(config.tunesLinkURL)
 
 export const processSong = async ({ id }) => {
+    id = id + "" // make it a string
     try {
         await dispatch.newFromTemplate("tunes-worker-*.service", id, {})
     } catch (error) {
