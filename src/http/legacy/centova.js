c8cc8fadef93844dac7bd34db51ecbeebaae8867
Leo Lam <leolino.lam@gmail.com>
2017-11-11 16:39:25 +0100

    Prevent Connect from setting arbitrary DNS records (#56)

--- src/http/legacy/centova.js
+++ src/http/legacy/centova.js
@@ -1,3 +1,4 @@
+import AccessDeniedError from "~/http/classes/AccessDeniedError"
 import BadRequestError from "~/http/classes/BadRequestError"
 const dns = requireFromRoot("components/coreos/dns.js")
 const nowPlayingHandler = requireFromRoot("components/nowplaying/handle.js")
@@ -25,11 +26,14 @@ export default ({ app, wrap }) => {
 
     app.post("/connect/addDNS/", wrap(async (req, res) => {
         if (!req.body.token) {
-            throw new BadRequestError("No token found in the request");
+            throw new AccessDeniedError("No token found in the request");
         }
         const valid = await timetoken.validateTokenForService("legacy-centova", req.body.token, 10)
         if (!valid) {
-            return res.status(400).json({ error: "invalid token" })
+            throw new AccessDeniedError("Invalid token");
+        }
+        if (!/([A-Za-z0-9_]+)\.radioca\.st/.test(req.body.name)) {
+            throw new AccessDeniedError("Invalid DNS name");
         }
         dns.setRecord(req.body.name, req.body.type, req.body.value, req.body.ttl)
         res.json({ status: "ok" })
