1a24d716ebe9071e6bf14ef7b10a7ea8fc5ece0b
Maartje Eyskens <meyskens@me.com>
2018-04-28 20:12:53 +0100

    filter PRs

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push innovate/itframe:latest
-docker tag innovate/itframe:latest innovate/itframe:$TRAVIS_COMMIT
-docker push innovate/itframe:$TRAVIS_COMMIT
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker push innovate/itframe:latest
+    docker tag innovate/itframe:latest innovate/itframe:$TRAVIS_COMMIT
+    docker push innovate/itframe:$TRAVIS_COMMIT
+fi
 if [ ! -z "$TRAVIS_TAG" ]; then 
     docker tag innovate/itframe:latest innovate/itframe:$TRAVIS_TAG
     docker push innovate/itframe:$TRAVIS_TAG
