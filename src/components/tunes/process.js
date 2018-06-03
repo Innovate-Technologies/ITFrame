cb98e4f4b7d68fe8cfd8519be47ee01f4d89afc6
Maartje Eyskens <meyskens@me.com>
2018-06-03 10:12:26 +0100

    Limit retries in tunes process

--- src/components/tunes/process.js
+++ src/components/tunes/process.js
@@ -1,20 +1,28 @@
 import { HelmetController } from "../helmet/controller.js"
 const controller = new HelmetController(config.tunesHelmetURL, config.tunesHelmetKey)
 
-export const processSong = async ({ id }) => {
+export const processSong = async ({ id }, attempt = 0) => {
     id = id + "" // make it a string
     try {
         await controller.create(id, { id })
     } catch (error) {
-        processSong({ id })
+        attempt++
+        if (attempt > 5) {
+            throw error
+        }
+        await processSong({ id }, attempt)
     }
 }
 
-export const stopContainer = async (id) => {
+export const stopContainer = async (id, attempt = 0) => {
     id = id + "" // make it a string
     try {
         await controller.destroy(id, { id })
     } catch (error) {
-        stopContainer(id)
+        attempt++
+        if (attempt > 5) {
+            throw error
+        }
+        await stopContainer(id, attempt)
     }
 }
