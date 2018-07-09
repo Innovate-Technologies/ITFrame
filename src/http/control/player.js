42a3d846a98779ef7cf7342a656995f26b206f09
Maartje Eyskens <meyskens@me.com>
2018-07-09 18:08:34 +0200

    promisify player in control

--- src/http/control/player.js
+++ src/http/control/player.js
@@ -1,4 +1,3 @@
-let wait = require("wait.for");
 let playerDatabase = requireFromRoot("components/player/database.js");
 import NotFoundError from "~/http/classes/NotFoundError";
 import * as nocover from "~/components/tunes/nocover";
@@ -27,31 +26,27 @@ module.exports = ({ app, wrap }) => {
         }
     });
 
-    app.put("/control/player/settings/:username", (req, res, next) => {
-        wait.launchFiber(function () {
-            try {
-                req.body.username = req.params.username;
-                wait.for(playerDatabase.upsertConfig, req.params.username, req.body);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to update config");
-                error.message = "Failed to update config: " + error.message;
-                return next(error);
-            }
-        });
+    app.put("/control/player/settings/:username", async (req, res, next) => {
+        try {
+            req.body.username = req.params.username;
+            await playerDatabase.upsertConfig(req.params.username, req.body);
+            res.json({});
+        } catch (error) {
+            req.log.warn(error, "Failed to update config");
+            error.message = "Failed to update config: " + error.message;
+            return next(error);
+        }
     });
 
-    app.delete("/control/player/settings/:username", function (req, res, next) {
-        wait.launchFiber(function () {
-            try {
-                wait.for(playerDatabase.removeConfig, req.params.username);
-                res.json({});
-            } catch (error) {
-                req.log.warn(error, "Failed to delete config");
-                error.message = "Failed to delete config: " + error.message;
-                return next(error);
-            }
-        });
+    app.delete("/control/player/settings/:username", async (req, res, next) => {
+        try {
+            await playerDatabase.removeConfig(req.params.username);
+            res.json({});
+        } catch (error) {
+            req.log.warn(error, "Failed to delete config");
+            error.message = "Failed to delete config: " + error.message;
+            return next(error);
+        }
     });
 
 };
