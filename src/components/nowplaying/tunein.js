3799f83445bb723da30f876a33dd287d36cd5ec9
Maartje Eyskens <meyskens@me.com>
2018-08-19 12:15:52 +0200

    Make tunein air use promises

--- src/components/nowplaying/tunein.js
+++ src/components/nowplaying/tunein.js
@@ -2,64 +2,65 @@ var database = require("./tuneinDatabase.js")
 var rest = require("restler")
 let moduleLogger = log.child({ component: "tunein" });
 
-module.exports.sendSong = function (username, title, artist) {
+module.exports.sendSong = async (username, title, artist) => {
     let logger = moduleLogger.child({ username });
-    database.getInfo(username, function (err, res) {
-        if (err && err.message !== "Username not in database") {
-            return logger.error(err);
+    let res
+    try {
+        res = await database.getInfo(username)
+    } catch (error) {
+        if (error && error.message !== "Username not in database") {
+            return logger.error(error);
         }
-        if (err) {
+        return
+    }
+    if (!res.isEnabled) {
+        return logger.debug("Skipping as isEnabled is falsy");
+    }
+    if (typeof title === "undefined" || title.length === 0) {
+        title = "Unknown"
+    }
+    if (typeof artist === "undefined" || artist.length === 0) {
+        artist = "Unknown"
+    }
+    let data = {
+        partnerId: res.partnerId.toString(),
+        partnerKey: res.partnerKey.toString(),
+        id: res.stationId,
+        title: title,
+        artist: artist,
+    };
+    logger = logger.child({ data });
+    logger.debug("Sending request");
+    rest.post("https://air.radiotime.com/Playing.ashx", {
+        data: data,
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "SHOUTca.st",
+        },
+    }).on("complete", function (response, info) {
+        if (response instanceof Error) {
+            logger.error(response, "Request to TuneIn API failed");
             return;
         }
-        if (!res.isEnabled) {
-            return logger.debug("Skipping as isEnabled is falsy");
-        }
-        if (typeof title === "undefined" || title.length === 0) {
-            title = "Unknown"
-        }
-        if (typeof artist === "undefined" || artist.length === 0) {
-            artist = "Unknown"
-        }
-        let data = {
-            partnerId: res.partnerId.toString(),
-            partnerKey: res.partnerKey.toString(),
-            id: res.stationId,
-            title: title,
-            artist: artist,
-        };
-        logger = logger.child({ data });
-        logger.debug("Sending request");
-        rest.post("https://air.radiotime.com/Playing.ashx", {
-            data: data,
-            timeout: 5000,
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "User-Agent": "SHOUTca.st",
-            },
-        }).on("complete", function (response, info) {
-            if (response instanceof Error) {
-                logger.error(response, "Request to TuneIn API failed");
-                return;
-            }
-            if (typeof response === "string" && response.includes("<status>403</status>")
-                    || info.statusCode === 403) {
-                // Not disabling the integration as TuneIn sends 403 randomly.
-                logger.warn("Got a 403, ignoring");
-                // logger.error("Got a 403 response, disabling integration");
-                // database.disable(username, "Invalid credentials. Please reconfigure the integration.");
-                return;
-            }
-            logger.debug({ response }, "Updated TuneIn listing");
-        }).on("403", function () {
+        if (typeof response === "string" && response.includes("<status>403</status>")
+                || info.statusCode === 403) {
             // Not disabling the integration as TuneIn sends 403 randomly.
             logger.warn("Got a 403, ignoring");
             // logger.error("Got a 403 response, disabling integration");
             // database.disable(username, "Invalid credentials. Please reconfigure the integration.");
-        });
-    })
+            return;
+        }
+        logger.debug({ response }, "Updated TuneIn listing");
+    }).on("403", function () {
+        // Not disabling the integration as TuneIn sends 403 randomly.
+        logger.warn("Got a 403, ignoring");
+        // logger.error("Got a 403 response, disabling integration");
+        // database.disable(username, "Invalid credentials. Please reconfigure the integration.");
+    });
 }
 
-module.exports.testInfo = (settings, callback) => {
+module.exports.testInfo = (settings) => new Promise((resolve, reject) => {
     let logger = moduleLogger.child({ username: settings.username });
     let data = {
         partnerId: settings.partnerId.toString(),
@@ -79,16 +80,16 @@ module.exports.testInfo = (settings, callback) => {
         },
     }).on("complete", function (response, info) {
         if (response instanceof Error) {
-            return callback(new Error("Invalid details provided for TuneIn AIR"))
+            return reject(new Error("Invalid details provided for TuneIn AIR"))
         }
         if (typeof response === "string" && response.includes("<status>403</status>")
                 || info.statusCode === 403) {
-            return callback(new Error("Invalid details provided for TuneIn AIR"));
+            return reject(new Error("Invalid details provided for TuneIn AIR"));
         }
-        callback()
+        return resolve()
     })
-}
+})
 
-module.exports.getAllUsers = function (callback) {
-    database.getAllUsers(callback)
+module.exports.getAllUsers = function () {
+    return database.getAllUsers()
 }
