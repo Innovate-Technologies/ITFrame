09f5617867a18dfa905eb1d86c855af0526aca17
Maartje Eyskens <maartje@eyskens.me>
2018-08-19 18:04:31 +0200

    Add Alexa app (#133)
    * Add alexa db
    
    * Add requests db
    
    * add alexa public API
    
    * Add request API with rate limiting
    
    * add alexa status
    
    * add control routes

---
+++ src/components/alexa/alexa.js
@@ -0,0 +1,52 @@
+const mongoose = requireFromRoot("components/database/mongodb.js");
+const Schema = mongoose.Schema;
+const AlexaSchema = new Schema({
+    username: String,
+    status: {
+        type: String,
+        default: "processing",
+        enum: [ "processing", "rejected", "active" ],
+    },
+    name: String,
+    logo: String,
+    languageEntries: [
+        {
+            language: {
+                type: String,
+                enum: ["en", "fr", "es", "de", "jp", "it"],
+            },
+            invocationName: String,
+            intro: String,
+            help: String,
+            shortDescription: String,
+            description: String,
+            keywords: [ String ],
+        },
+    ],
+}, { collection: "alexa"});
+AlexaSchema.index({
+    username: 1,
+});
+const AlexaModel = mongoose.model("alexa", AlexaSchema, "alexa");
+
+export const entryForUsername = (username) => {
+    return AlexaModel.findOne({
+        username,
+    }).exec()
+}
+
+export const newForUsername = async (username, info) => {
+    info.username = username
+    return (new AlexaModel(info)).save()
+}
+
+export const updateForUsername = async (username, info) => {
+    info.username = username
+    return AlexaModel.update({
+        username: username,
+    }, info).exec()
+}
+
+export const deleteForUsername = async (username) => {
+    return AlexaModel.remove({ username }).exec()
+}
---
+++ src/http/control/alexa.js
@@ -0,0 +1,28 @@
+import * as alexa from "~/components/alexa/alexa"
+import NotFoundError from "~/http/classes/NotFoundError";
+
+export default function ({ app, wrap }) {
+    app.get("/control/alexa/settings/:username", wrap(async function (req, res) {
+        const entry = await alexa.entryForUsername(req.params.username)
+        if (entry === null) {
+            throw new NotFoundError("No Slexa app for username found")
+        }
+        res.json(entry)
+    }));
+
+    app.put("/control/alexa/settings/:username", wrap(async function (req, res) {
+        let settings = req.body || {};
+        const entry = await alexa.entryForUsername(req.params.username)
+        if (entry === null) {
+            await alexa.newForUsername(req.params.username, settings)
+        } else {
+            await alexa.updateForUsername(req.params.username, settings);
+        }
+        res.json({});
+    }));
+
+    app.delete("/control/alexa/settings/:username", wrap(async function (req, res) {
+        await alexa.deleteForUsername(req.params.username);
+        res.json({});
+    }));
+}
