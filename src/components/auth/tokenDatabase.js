4b02b15063a845d0e9d314d7d0e1842b0c7ae9fa
Maartje Eyskens <maartje@eyskens.me>
2018-08-19 12:16:20 +0200

    rm time tokens (#145)

--- src/components/auth/tokenDatabase.js
+++ src/components/auth/tokenDatabase.js
@@ -1,12 +1,12 @@
 const mongoose = require(global.appRoot + "/components/database/mongodb.js");
 const Schema = mongoose.Schema;
-const timetokensSchema = new Schema({ service: String, key: String });
-const timetokensModel = mongoose.model("time_tokens", timetokensSchema, "time_tokens");
+const tokensSchema = new Schema({ service: String, key: String });
+const tokensModel = mongoose.model("time_tokens", tokensSchema, "time_tokens");
 const moduleLogger = log.child({ component: "timetoken" });
 
 export const getAPIKey = async (service) => {
     const logger = moduleLogger.child({ service });
-    const info = await timetokensModel.findOne({ service }).exec()
+    const info = await tokensModel.findOne({ service }).exec()
     if (info === null) {
         logger.warn({ result: info }, "No match in the time token model, access denied");
         throw new Error("No entry for the service")
