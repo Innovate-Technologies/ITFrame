cd76ea30cb2ddbfba28f0fb79878cc6ee85aebcc
Maartje Eyskens <meyskens@me.com>
2018-08-19 11:28:50 +0200

    rm time tokens

--- src/components/auth/timetoken.js
+++ src/components/auth/timetoken.js
@@ -1,33 +1,9 @@
-import speakeasy from "speakeasy"
-import * as database from "./timetokenDatabase.js"
+import * as database from "./tokenDatabase.js"
 
-export const generateTokenForService = async (service) => {
+export const validateTokenForService = async (service, token) => {
     const info = await database.getAPIKey(service)
-    return speakeasy.hotp({
-        key: info.key,
-        counter: Math.round((new Date()).getTime() / 1000),
-    })
-}
-
-export const validateTokenForService = async (service, token, offset = 1) => {
-    const info = await database.getAPIKey(service)
-    if (token === info.key) { // if server sends the key it is fine too as tokens seem to cause issues sometimes
+    if (token === info.key) {
         return true
     }
-
-    const time = Math.round((new Date()).getTime() / 1000)
-    const volidTokens = []
-
-    for (let i = 0; i < (offset + 1); i++) {
-        volidTokens.push(speakeasy.hotp({
-            key: info.key,
-            counter: time + i,
-        }))
-        volidTokens.push(speakeasy.hotp({
-            key: info.key,
-            counter: time - i,
-        }))
-    }
-
-    return volidTokens.indexOf(token) !== -1
+    return false
 }
