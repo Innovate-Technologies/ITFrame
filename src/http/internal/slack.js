caa678e14274e53d65a0644655e8097ccc493197
Maartje Eyskens <meyskens@me.com>
2018-08-22 12:33:13 +0200

    test return of promise

--- src/http/internal/slack.js
+++ src/http/internal/slack.js
@@ -16,26 +16,24 @@ export default ({ app }) => {
             return
         }
 
-        respond({})
-
         if (payload.actions[0].name === "Reject invocation name") {
-            await alexa.updateForUsername(payload.actions[0].value, { status: "rejected", rejectReason: "The invocation name text does not comply to Amazon's specifications" })
             await slack.disableButtons(payload.original_message, `:heavy_multiplication_x: rejected by <@${payload.user.id}>`)
+            return alexa.updateForUsername(payload.actions[0].value, { status: "rejected", rejectReason: "The invocation name text does not comply to Amazon's specifications" })
         }
 
         if (payload.actions[0].name === "Reject intro") {
-            await alexa.updateForUsername(payload.actions[0].value, { status: "rejected", rejectReason: "The intro text does not comply to Amazon's specifications" })
-            await slack.disableButtons(payload.original_message, `:heavy_multiplication_x: rejected by <@${payload.user.id}>`);
+            await slack.disableButtons(payload.original_message, `:heavy_multiplication_x: rejected by <@${payload.user.id}>`)
+            return alexa.updateForUsername(payload.actions[0].value, { status: "rejected", rejectReason: "The intro text does not comply to Amazon's specifications" })
         }
 
         if (payload.actions[0].name === "Reject help") {
-            await alexa.updateForUsername(payload.actions[0].value, { status: "rejected", rejectReason: "The help text does not comply to Amazon's specifications" })
-            await slack.disableButtons(payload.original_message, `:heavy_multiplication_x: rejected by <@${payload.user.id}>`);
+            await slack.disableButtons(payload.original_message, `:heavy_multiplication_x: rejected by <@${payload.user.id}>`)
+            return alexa.updateForUsername(payload.actions[0].value, { status: "rejected", rejectReason: "The help text does not comply to Amazon's specifications" })
         }
 
         if (payload.actions[0].name === "Accept") {
-            await alexa.updateForUsername(payload.actions[0].value, { status: "approved" })
             await slack.disableButtons(payload.original_message, `:heavy_check_mark: approved by <@${payload.user.id}>`)
+            return alexa.updateForUsername(payload.actions[0].value, { status: "approved" })
         }
     });
 }
