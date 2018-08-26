4f6361a074120fc395a65b09f8146aa98fc02abd
Maartje Eyskens <meyskens@me.com>
2018-08-26 15:46:58 +0200

    use .count

--- src/components/tunes/personalMusicDatabase.js
+++ src/components/tunes/personalMusicDatabase.js
@@ -135,7 +135,7 @@ export const getSongsForUser = async (username, itemsPerPage, page = 1, sortBy) 
 }
 
 export const getSongCountsForUser = async (username) => {
-    return TunesPersonalModel.countDocuments({ username, type: "song" }).exec()
+    return TunesPersonalModel.count({ username, type: "song" }).exec()
 }
 
 export const getSongsForUserWithTag = (username, tag) => {
