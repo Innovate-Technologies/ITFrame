9238a672fedacdce81cf415c07b80fa38ce3f146
Maartje Eyskens <meyskens@me.com>
2018-12-23 11:07:23 +0100

    Replace lwip with sharp
    Signed-off-by: Maartje Eyskens <meyskens@me.com>

--- src/components/bufferImageResize.js
+++ src/components/bufferImageResize.js
@@ -1,34 +1,22 @@
-let lwip = require("lwip");
+const sharp = require("sharp");
 
 /**
  * Resize a PNG image contained in a Buffer
  * @param  {Buffer}   buffer         Buffer containing the image
- * @param  {String}   format         Image format (png, jpg, gif)
+ * @param  {String}   format         Image format (png, jpg)
  * @param  {Integer}  options.width  Target width
  * @param  {Integer}  options.height Target height
  * @param  {Function} callback       Callback function (err, buffer)
  */
 export default function resizeImage(buffer, format, { width, height }, callback) {
-    let openCallback = (err, image) => {
-        if (err) {
-            return callback("Failed to open the image: " + err);
-        }
-        image.resize(width, height, resizeCallback);
-    };
+    let image = sharp(buffer)
+    if (format == "png") {
+        image = image.png()
+    } else if (format == "jpg") {
+        image = image.jpeg()
+    }
 
-    let resizeCallback = (err, image) => {
-        if (err) {
-            return callback("Failed to resize the image: " + err);
-        }
-        image.toBuffer("png", toBufferCallback);
-    };
+    image= image.resize({ width, height })
 
-    let toBufferCallback = (err, resizedBuffer) => {
-        if (err) {
-            return callback("Failed to get the resized image: " + err);
-        }
-        return callback(null, resizedBuffer);
-    };
-
-    lwip.open(buffer, format, openCallback);
+    return callback(null, image.toBuffer());
 }
