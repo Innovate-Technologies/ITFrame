d70c30822d66d08546f627e19d80dab22862af5d
Arron Auchterlounie <arron@unmuted.ltd>
2021-04-13 17:10:04 +0100

    fix (build): forgot {} in downloadGeo.sh

--- downloadGeo.sh
+++ downloadGeo.sh
@@ -8,8 +8,8 @@ cd $MAXMIND_DB_DIR
 # wget http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz || { echo 'Could not download GeoIPASNum, exiting.' ; exit 1; }
 # wget http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz || { echo 'Could not download GeoIPASNumv6, exiting.' ; exit 1; }
 
-wget https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=$GEOLITE_KEY&suffix=tar.gz || { echo 'Could not download GeoLite2 Country, exiting.' ; exit 1; }
-wget https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$GEOLITE_KEY&suffix=tar.gz || { echo 'Could not download GeoLite2 City, exiting.' ; exit 1; }
-wget https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$GEOLITE_KEY&suffix=tar.gz || { echo 'Could not download GeoLite2 ASN, exiting.' ; exit 1; }
+wget https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${GEOLITE_KEY}&suffix=tar.gz || { echo 'Could not download GeoLite2 Country, exiting.' ; exit 1; }
+wget https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${GEOLITE_KEY}&suffix=tar.gz || { echo 'Could not download GeoLite2 City, exiting.' ; exit 1; }
+wget https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=${GEOLITE_KEY}&suffix=tar.gz || { echo 'Could not download GeoLite2 ASN, exiting.' ; exit 1; }
 
 tar -xzvf *.tar.gz
